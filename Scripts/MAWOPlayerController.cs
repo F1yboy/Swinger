7662761337b9c73ba0f694dd4e342dc582eb12c7
F1yboy <42953928+F1yboy@users.noreply.github.com>
2018-12-01 22:08:22 -0500

    Add files via upload

---
+++ Scripts/MAWOPlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Text timerText;
+    public Text winText;
+    public Text endText;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        winText.text = "";
+        endText.text = "";
+        timerText.text = "";
+
+    }
+    void FixedUpdate()
+    {
+        timer = timer + Time.deltaTime;
+        if (timer >= 10)
+        {
+            endText.text = "You Lose! :(";
+            StartCoroutine(ByeAfterDelay(2));
+        }
+
+    }
+
+    IEnumerator ByeAfterDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+        GameLoader.gameOn = false;
+    }
+   
+    void SetCountText()
+    {
+        if (count >= 10)
+        {
+            endText.text = "You win!";
+            StartCoroutine(ByeAfterDelay(2));
+        }
+        GameLoader.AddScore(10);
+    }
+}
